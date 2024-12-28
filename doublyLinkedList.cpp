94d291a3198ccd9c6e6070198c11d0a4b141eaa0
ayaz <ayaz17sheikh@gmail.com>
2024-12-28 12:09:09 +0530

    doubly linked list done

---
+++ doublyLinkedList.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    // constructor
+    Node(int d)
+    {
+        this->data = d;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int getLength(Node *head)
+{
+    int len = 0;
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+
+void insertAtHead(Node *&head, Node *&tail, int d)
+{
+    if (head == NULL)
+    {
+        Node *temp = new Node(d);
+        temp = head;
+        temp = tail;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+
+void insertAtTail(Node *&tail, Node *&head, int d)
+{
+    if (tail == NULL)
+    {
+        Node *temp = new Node(d);
+        temp = tail;
+        temp = head;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        tail->next = temp;
+        temp->prev = tail;
+        tail = temp;
+    }
+}
+
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+
+    if (position == 1)
+    {
+        insertAtHead(head, tail, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, head, d);
+        return;
+    }
+
+    // creating a node for d
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next->prev = nodeToInsert;
+    temp->next = nodeToInsert;
+    nodeToInsert->prev = temp;
+}
+
+int main()
+{
+    // Node *node1 = new Node(10);
+    Node *head = NULL;
+    Node *tail = NULL;
+    cout << getLength(head);
+    return 0;
+}
